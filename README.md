481d7d57c35ee095574d7388e591e436507811a8
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-18 11:54:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# label merge-conflict action
+
+This action adds a given label to Pull Requests that have merge conflicts and removes a given label from these pull requests
+
+## Inputs
+
+### `dirtyLabel`
+
+**Required** The name of the label that should be added once a PR has merge conflicts.
+
+### `removeOnDirtyLabel`
+
+**Required** The name of the label that should be removed once a PR has merge conflicts.
+
+### `repoToken`
+
+**Required** Token for the repository. Can be passed in using {{ secrets.GITHUB_TOKEN }}
+
+## Example usage
+
+```yaml
+steps:
+  - name: check if prs are dirty
+    uses: eps1lon/actions-label-merge-conflict@releases/1.x
+    with:
+      dirtyLabel: "PR: needs rebase"
+      removeOnDirtyLabel: "PR: ready to ship"
+      repoToken: "${{ secrets.GITHUB_TOKEN }}"
+```
---
+++ sources/main.ts
@@ -0,0 +1,159 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function main() {
+	const repoToken = core.getInput("repoToken", { required: true });
+	const dirtyLabel = core.getInput("dirtyLabel", { required: true });
+	const removeOnDirtyLabel = core.getInput("removeOnDirtyLabel", {
+		required: true
+	});
+
+	const client = new github.GitHub(repoToken);
+
+	return await checkDirty({
+		client,
+		dirtyLabel,
+		removeOnDirtyLabel,
+		after: null
+	});
+}
+
+interface CheckDirtyContext {
+	after: string | null;
+	client: github.GitHub;
+	dirtyLabel: string;
+	removeOnDirtyLabel: string;
+}
+async function checkDirty(context: CheckDirtyContext): Promise<void> {
+	const { after, client, dirtyLabel, removeOnDirtyLabel } = context;
+
+	interface RepositoryResponse {
+		repository: any;
+	}
+	const query = `
+query openPullRequests($owner: String!, $repo: String!, $after: String) { 
+  repository(owner:$owner, name: $repo) { 
+    pullRequests(first:100, after:$after, states: OPEN) {
+      nodes {
+        mergeable
+        number
+        permalink
+        title
+        updatedAt
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+}
+  `;
+	core.debug(query);
+	const pullsResponse = await client.graphql(query, {
+		headers: {
+			// merge-info preview causes mergeable to become "UNKNOW" (from "CONFLICTING")
+			// kind of obvious to no rely on experimental features but...yeah
+			//accept: "application/vnd.github.merge-info-preview+json"
+		},
+		after,
+		owner: github.context.repo.owner,
+		repo: github.context.repo.repo
+	});
+
+	const {
+		repository: {
+			pullRequests: { nodes: pullRequests, pageInfo }
+		}
+	} = pullsResponse as RepositoryResponse;
+	core.debug(JSON.stringify(pullsResponse, null, 2));
+
+	if (pullRequests.length === 0) {
+		return;
+	}
+
+	for (const pullRequest of pullRequests) {
+		core.debug(JSON.stringify(pullRequest, null, 2));
+
+		const info = (message: string) =>
+			core.info(`for PR "${pullRequest.title}": ${message}`);
+
+		switch (pullRequest.mergeable) {
+			case "CONFLICTING":
+				info(`add "${dirtyLabel}", remove "${removeOnDirtyLabel}"`);
+				// for labels PRs and issues are the same
+				await Promise.all([
+					addLabel(dirtyLabel, pullRequest, { client }),
+					removeLabelIfExists(removeOnDirtyLabel, pullRequest, { client })
+				]);
+				break;
+			case "MERGEABLE":
+				info(`remove "${dirtyLabel}"`);
+				await removeLabelIfExists(dirtyLabel, pullRequest, { client });
+				// while we removed a particular label once we enter "CONFLICTING"
+				// we don't add it again because we assume that the removeOnDirtyLabel
+				// is used to mark a PR as "merge!".
+				// So we basically require a manual review pass after rebase.
+				break;
+			case "UNKNOWN":
+				info(`do nothing`);
+				break;
+			default:
+				throw new TypeError(
+					`unhandled mergeable state '${pullRequest.mergeable}'`
+				);
+		}
+	}
+
+	if (pageInfo.hasNextPage) {
+		return checkDirty({
+			...context,
+			after: pageInfo.endCursor
+		});
+	}
+}
+
+function addLabel(
+	label: string,
+	{ number }: { number: number },
+	{ client }: { client: github.GitHub }
+) {
+	return client.issues
+		.addLabels({
+			owner: github.context.repo.owner,
+			repo: github.context.repo.repo,
+			issue_number: number,
+			labels: [label]
+		})
+		.catch(error => {
+			throw new Error(`error adding "${label}": ${error}`);
+		});
+}
+
+function removeLabelIfExists(
+	label: string,
+	{ number }: { number: number },
+	{ client }: { client: github.GitHub }
+) {
+	return client.issues
+		.removeLabel({
+			owner: github.context.repo.owner,
+			repo: github.context.repo.repo,
+			issue_number: number,
+			name: label
+		})
+		.catch(error => {
+			if (error.status !== 404) {
+				throw new Error(`error removing "${label}": ${error}`);
+			} else {
+				core.info(
+					`On #${number} label "${label}" doesn't need to be removed since it doesn't exist on that issue.`
+				);
+			}
+		});
+}
+
+main().catch(error => {
+	core.error(String(error));
+	core.setFailed(String(error.message));
+});
